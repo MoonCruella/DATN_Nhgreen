e4bfa83c8364f850f13ccf05fdb1fdb4236f6cea
MunCruella <nhunguyetpy206@gmail.com>
2026-07-11 20:47:10 +0700

    Fix deploy Redis #2

--- server/config/redisClient.js
+++ server/config/redisClient.js
@@ -1,8 +1,6 @@
 import { createClient } from "redis";
 
 const buildRedisUrl = () => {
-  if (process.env.REDIS_URL) return process.env.REDIS_URL;
-
   const host = process.env.REDIS_HOST || "127.0.0.1";
   const port = process.env.REDIS_PORT || "6379";
   const username = process.env.REDIS_USERNAME || "";
